47515c83fea9383b09ab897acf518a0c14c4a7b1
RodroVMS <democlesvanshepars@gmail.com>
2021-11-04 12:12:44 -0400

    add class person, male and female
    Person is ancestor class of both male and female. The difference betwwen
    male and female is that one can have kids and get pregnant and the other
    cannot.

---
+++ classes.py
@@ -0,0 +1,109 @@
+class Person:
+    def __init__(self, name: str, age: int, max_kids: int) -> None:
+        self.name: str = name
+        self.age: int = age
+        self.max_kids: int = max_kids
+        self._wants_partner: bool = False
+        self._partnered: bool = False
+        self._partner: Person
+        self._time_alone: int = 0
+        self._children: int = 0
+
+    @property
+    def wants_partner(self):
+        return self._wants_partner
+
+    @property
+    def has_partner(self):
+        return self._partnered
+
+    @property
+    def get_partner(self):
+        return self._partner
+
+    @property
+    def want_kids(self):
+        return self._children < self.max_kids
+
+    def set_wants_partner(self):
+        if self._wants_partner:
+            raise Exception(f"{self.name} already wants a partner.")
+        if self._partnered:
+            raise Exception(f"{self.name} already has a partner.")
+        if self._time_alone != 0:
+            raise Exception(f"{self.name} is recovering from past relationships.")
+        self._wants_partner = True
+
+    def set_partner(self, partner):
+        if self._partnered:
+            raise Exception(
+                f"{self.name} is already partnered with {self._partner.name}"
+            )
+        if not self._wants_partner:
+            raise Exception(f"{self.name} is not looking for a partner")
+        if (self.is_female and partner.is_female) or (self.is_male and partner.is_male):
+            raise Exception(f"Simulation does not aloud same sex relationships")
+        self._partnered = True
+        self._partner: Person = partner
+
+    def break_up(self, time: int) -> None:
+        if not self._partnered:
+            raise Exception(f"{self.name} cannot break up without a partner")
+        self.update_time_alone = time
+
+    def update_time_alone(self, time: int) -> int:
+        self._time_alone = max(self._time_alone - time, 0)
+        return self._time_alone
+
+    @property
+    def is_female(self):
+        raise NotImplementedError()
+
+    @property
+    def is_male(self):
+        raise NotImplementedError()
+
+
+class Female(Person):
+    def __init__(self, name: str, age: int, max_kids: int) -> None:
+        super().__init__(name, age, max_kids)
+        self._pregnant_kids = 0
+
+    @property
+    def is_pregnant(self):
+        return self._pregnant_kids > 0
+
+    def set_pregnant(self, kids_count: int):
+        if not self.has_partner:
+            raise Exception(f"{self.name} is not partenred with anyone")
+        if self.is_pregnant:
+            raise Exception(f"{self.name} is already pregnant")
+        if not self.want_kids:
+            raise Exception(f"{self.name} want's no more kids")
+        self._pregnant_kids = kids_count
+
+    def labour(self) -> int:
+        new_little_people = self._pregnant_kids
+        self._pregnant_kids = 0
+        return new_little_people
+
+    @property
+    def is_female(self):
+        return True
+
+    @property
+    def is_male(self):
+        return False
+
+
+class Male(Person):
+    def __init__(self, name: str, age: int, max_kids: int) -> None:
+        super().__init__(name, age, max_kids)
+
+    @property
+    def is_female(self):
+        return False
+
+    @property
+    def is_male(self):
+        return True
