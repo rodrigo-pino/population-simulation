48e6b0712007e601bf795ab65db816f93c0c67db
RodroVMS <democlesvanshepars@gmail.com>
2021-11-21 12:42:16 -0500

    separte constants

---
+++ src/config.py
@@ -0,0 +1,11 @@
+# Determina el fin de la simulacion
+SIMULATION_END = 100 * 12
+# Los rangos de edades de las personas
+AGE_RANK = [i * 12 for i in [12, 15, 21, 35, 45, 60, 125, 200]]
+# Los rangos en el tiempo en los que se programan los eventos
+# de 1 a 6 meses en adelante, etc...
+PREGNANT_RANGE = (1, 6)
+PREGNANT_REST_RANGE = (3, 12)
+PARTNER_RANGE = (1, 3)
+WANT_PARTNER_RANGE = (1, 12)
+BREAK_UP_RANGE = (6, 18)
